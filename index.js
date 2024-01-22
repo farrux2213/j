151d828103d87bbffa00570a5dc84e56cab0f97d
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-01-22 23:17:45 +0500

    bciid

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log(Math.floor(202.49999));
 console.log(Math.pow(20, 3.2));
-console.log(Math.pow(Math.random() * 22), 2);
 console.log(Math.random());
 console.log(Math.random() * 22);
 let www = Math.random() * 22;
 console.log(Math.floor(www));
+console.log(Math.pow(Math.floor(www), 2));
